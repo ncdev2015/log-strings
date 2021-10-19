39220114342cdf391a1d26ad06c916666b1e6229
ncdev2015 <ncdev2015@gmail.com>
2021-10-19 14:21:29 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace log_de_strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = 1, n2 = 2, n3 = 3, n4 = 4;
+
+            Console.WriteLine("N1: {0}, N2: {1}, N3: {2}, N4: {3}", n1, n2, n3, n4);
+            //Console.WriteLine();
+            //Console.WriteLine("N1: {0}, N2: {1}, N4: {3}", n1, n2, n4); // Error: Index based
+            Console.WriteLine();
+            Console.WriteLine("N1: {0}, N2: {1}, N4: {2}", n1, n2, n4);
+        }
+    }
+}
