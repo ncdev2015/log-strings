c9a4fa8c75a49e2de243fa26e9f7c578d17f42cb
ncdev2015 <ncdev2015@gmail.com>
2021-10-20 11:28:32 -0300

    Added: comments to describe use

--- Program.cs
+++ Program.cs
@@ -8,12 +8,14 @@ namespace log_strings
         {
             int n1 = 1, n2 = 2, n3 = 3, n4 = 4;
 
+            // Composite formatting
             Console.WriteLine("N1: {0}, N2: {1}, N3: {2}, N4: {3}", n1, n2, n3, n4);
             //Console.WriteLine();
             //Console.WriteLine("N1: {0}, N2: {1}, N4: {3}", n1, n2, n4); // Error: Index based, test it
             Console.WriteLine();
             Console.WriteLine("N1: {0}, N2: {1}, N4: {2}", n1, n2, n4);
             Console.WriteLine();
+            // String interpolation:
             Console.WriteLine($"N1: {n1}, N2: {n2}, N3: {n3}, N4: {n4}");
         }
     }
